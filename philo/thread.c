b7114e975b43bc78e83bc2c32be4cce222e42599
Oriol Jimenez Garrich <ojimenez@cbr6s5.42barcelona.com>
2023-11-15 16:49:33 +0100

    Cal arreglar

--- philo/thread.c
+++ philo/thread.c
@@ -6,7 +6,7 @@
 /*   By: ojimenez <ojimenez@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 15:55:08 by ojimenez          #+#    #+#             */
-/*   Updated: 2023/11/15 13:00:37 by ojimenez         ###   ########.fr       */
+/*   Updated: 2023/11/15 16:47:29 by ojimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,14 @@ void	*monitor(void *p)
 	philo = (t_philo *)p;
 	while (philo->data->dead == 0)
 	{
+
 		pthread_mutex_lock(&philo->lock);
 		if (philo->data->all_eaten >= philo->data->num_philo)
+		{
+			pthread_mutex_lock(&philo->data->lock);
 			philo->data->dead = 1;
+			pthread_mutex_unlock(&philo->data->lock);
+		}
 		pthread_mutex_unlock(&philo->lock);
 	}
 	return ((void *)0);
@@ -75,25 +80,25 @@ int	init_threads(t_data *data)
 	int			i;
 	pthread_t	thread;
 
-	i = 0;
+	i = -1;
 	data->start_time = get_time();
 	if (data->number_of_eats > 0)
 	{
 		if (pthread_create(&thread, NULL, &monitor, &data->philos[0]))
 			return (destroy_all(data));
 	}
-	while (i < data->num_philo)
+	while (++i < data->num_philo)
 	{
 		if (pthread_create(&data->t_id[i], NULL, &th_routine, &data->philos[i]))
 			return (destroy_all(data));
-		i++;
+		if (i % 2 == 0)
+			usleep(10);
 	}
-	i = 0;
-	while (i < data->num_philo)
+	i = -1;
+	while (++i < data->num_philo)
 	{
 		if (pthread_join(data->t_id[i], NULL))
 			return (destroy_all(data));
-		i++;
 	}
 	return (0);
 }
